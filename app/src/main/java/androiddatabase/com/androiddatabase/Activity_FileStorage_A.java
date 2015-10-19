7ad5d6038b9ad8f2af9f024c70e7171cc6a5d6b1
dev-bpn <jazzbpn@gmail.com>
2015-10-19 15:36:49 +0545

    Save File

--- app/src/main/java/androiddatabase/com/androiddatabase/Activity_FileStorage_A.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/Activity_FileStorage_A.java
@@ -1,10 +1,15 @@
 package androiddatabase.com.androiddatabase;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Activity_FileStorage_A extends AppCompatActivity {
 
@@ -19,6 +24,25 @@ public class Activity_FileStorage_A extends AppCompatActivity {
 
     public void save(View view){
 
+        String username = name.getText().toString();
+        String pass = password.getText().toString();
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = openFileOutput("FILE_NAME.txt" , Context.MODE_PRIVATE);
+            fileOutputStream.write(username.getBytes());
+            fileOutputStream.write(pass.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
