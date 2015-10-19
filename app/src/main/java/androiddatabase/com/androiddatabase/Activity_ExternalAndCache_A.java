e5755f5810432de820bc4b8e92cb53008497b110
dev-bpn <jazzbpn@gmail.com>
2015-10-19 18:11:53 +0545

    Save to Internal/External/private/public CACHE

--- app/src/main/java/androiddatabase/com/androiddatabase/Activity_ExternalAndCache_A.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/Activity_ExternalAndCache_A.java
@@ -2,40 +2,94 @@ package androiddatabase.com.androiddatabase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Activity_ExternalAndCache_A extends AppCompatActivity {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__external_and_cache_a);
+        editText = (EditText) findViewById(R.id.editText_a);
+    }
+
+    public void writeData(File file , String data){
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(data.getBytes());
+            displayToast(data + " is successfully written  to "+ file.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            Log.i("FILE_ERROR" , "FileNotFoundException");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.i("FILE_ERROR" , "IOException");
+            e.printStackTrace();
+        }finally {
+            try {
+                if(fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                Log.i("FILE_ERROR" , "IOException");
+                e.printStackTrace();
+            }
+        }
     }
 
     public void internalCache(View view){
 
-        Toast.makeText(this , "Save in InternalCache" , Toast.LENGTH_LONG).show();
+        String data = editText.getText().toString();
+        File folder = getCacheDir();
+        File file = new File(folder , "data1.txt");
+        writeData(file, data);
+
     }
 
     public void externalCache(View view){
 
-        Toast.makeText(this , "Save in externalCache" , Toast.LENGTH_LONG).show();
+        String data = editText.getText().toString();
+        File folder = getExternalCacheDir();
+        File file = new File(folder , "data2.txt");
+        writeData(file, data);
+
     }
 
     public void externalPrivate(View view){
 
-        Toast.makeText(this , "Save in externalPrivate" , Toast.LENGTH_LONG).show();
+        String data = editText.getText().toString();
+        File folder = getExternalFilesDir("Bpn");
+        File file = new File(folder , "data3.txt");
+        writeData(file, data);
+
     }
 
     public void externalPublic(View view){
 
-        Toast.makeText(this , "Save in externalPublic" , Toast.LENGTH_LONG).show();
+        String data = editText.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(folder , "data4.txt");
+        writeData(file, data);
+
     }
 
     public void next(View view){
         startActivity(new Intent(this , Activity_ExternalAndCache_B.class));
         Toast.makeText(this , "Next" , Toast.LENGTH_LONG).show();
+    }
+
+    private void displayToast(String message){
+        Toast.makeText(this , message , Toast.LENGTH_LONG).show();
     }
 }
