0aa9bc5e7f296b65e4ce527a34953ed6b882ae75
dev-bpn <jazzbpn@gmail.com>
2015-10-20 14:47:29 +0545

    Issue solved for onCreate

--- app/src/main/java/androiddatabase/com/androiddatabase/MainActivity.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/MainActivity.java
@@ -1,19 +1,25 @@
 package androiddatabase.com.androiddatabase;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androiddatabase.com.androiddatabase.SQLite.MySQLiteHelper;
+
 public class MainActivity extends AppCompatActivity {
 
+    MySQLiteHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        helper = new MySQLiteHelper(this);
+        SQLiteDatabase sqLiteDatabase = helper.getWritableDatabase();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/androiddatabase/com/androiddatabase/SQLite/MySQLiteHelper.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/SQLite/MySQLiteHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by Dell on 10/20/2015.
  */
-public class SQLiteHelper extends SQLiteOpenHelper {
+public class MySQLiteHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "my_database";
     private static final String TABLE_NAME = "my_table";
@@ -17,20 +17,22 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     private static final String NAME = "Name";
 
     private Context context;
-    private static final String CREATE_TABLE= "CREATE TABLE "+TABLE_NAME+" ("+UID+" PRIMARY KEY AUTOINCREMENT , "+NAME+" Varchar(255));";
+    private static final String CREATE_TABLE= "CREATE TABLE "+TABLE_NAME+" ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" Varchar(255));";
     private static final String DROP_TABLE = "DROP TABLE "+TABLE_NAME+"IF EXISTS";
-    public SQLiteHelper(Context context) {
+    public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
+        DisplayMessage.message(context , "Constructor is called");
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         // CREATE TABLE TABLE_NAME (_id PRIMARY KEY AUTOINCREMENT , Name Varchar(255));
         try {
+            DisplayMessage.message(context , "onCreate is called");
             sqLiteDatabase.execSQL(CREATE_TABLE);
         } catch (SQLException e) {
-            DisplayMessage.message(context , ""+e);
+            DisplayMessage.message(context , "EXCEPTION: "+e);
             e.printStackTrace();
         }
     }
@@ -38,10 +40,11 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         try {
+            DisplayMessage.message(context , "onUpgrade is called");
             sqLiteDatabase.execSQL(DROP_TABLE);
             onCreate(sqLiteDatabase);
         } catch (SQLException e) {
-            DisplayMessage.message(context , ""+e);
+            DisplayMessage.message(context , "EXCEPTION: "+e);
             e.printStackTrace();
         }
     }
