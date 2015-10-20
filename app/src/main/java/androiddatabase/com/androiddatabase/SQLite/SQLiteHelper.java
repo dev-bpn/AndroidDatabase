707e36d0f69ba335b4707cab35ac0a5e66e3945a
dev-bpn <jazzbpn@gmail.com>
2015-10-20 13:22:11 +0545

    onUpgrade Database

--- app/src/main/java/androiddatabase/com/androiddatabase/SQLite/SQLiteHelper.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/SQLite/SQLiteHelper.java
@@ -16,9 +16,9 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     private static final String UID = "_id";
     private static final String NAME = "Name";
 
-    private static final String CREATE_TABLE= "CREATE TABLE "+TABLE_NAME+" ("+UID+" PRIMARY KEY AUTOINCREMENT , "+NAME+" Varchar(255));";
     private Context context;
-
+    private static final String CREATE_TABLE= "CREATE TABLE "+TABLE_NAME+" ("+UID+" PRIMARY KEY AUTOINCREMENT , "+NAME+" Varchar(255));";
+    private static final String DROP_TABLE = "DROP TABLE "+TABLE_NAME+"IF EXISTS";
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
@@ -36,7 +36,13 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        try {
+            sqLiteDatabase.execSQL(DROP_TABLE);
+            onCreate(sqLiteDatabase);
+        } catch (SQLException e) {
+            DisplayMessage.message(context , ""+e);
+            e.printStackTrace();
+        }
     }
 }
