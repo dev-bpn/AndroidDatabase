548f4fda8c6423d4fe87ac30a339614f6fe7c5e5
dev-bpn <jazzbpn@gmail.com>
2015-10-20 13:17:18 +0545

    onCreate Database

--- app/src/main/java/androiddatabase/com/androiddatabase/SQLite/SQLiteHelper.java
+++ app/src/main/java/androiddatabase/com/androiddatabase/SQLite/SQLiteHelper.java
@@ -1,6 +1,7 @@
 package androiddatabase.com.androiddatabase.SQLite;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,14 +16,23 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     private static final String UID = "_id";
     private static final String NAME = "Name";
 
+    private static final String CREATE_TABLE= "CREATE TABLE "+TABLE_NAME+" ("+UID+" PRIMARY KEY AUTOINCREMENT , "+NAME+" Varchar(255));";
+    private Context context;
 
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        // CREATE TABLE TABLE_NAME (_id PRIMARY KEY AUTOINCREMENT , Name Varchar(255));
+        try {
+            sqLiteDatabase.execSQL(CREATE_TABLE);
+        } catch (SQLException e) {
+            DisplayMessage.message(context , ""+e);
+            e.printStackTrace();
+        }
     }
 
     @Override
